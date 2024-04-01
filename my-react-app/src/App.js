7ce20e43b5c270fc431ddc55ce806e936c046cbf
rrronit <ronitsingh.cool4@gmail.com>
2024-04-01 18:45:35 +0530

    add pages

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<P />} />
+          <Route path='/a' element={<A />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+const P = () => {
+  return (
+    <div>page P
+      <br />
+      <Link to={"/a"}>Go to A</Link>
+    </div>
+  )
+}
+
+
+
+
+
+const A = () => {
+  return (
+    <div>page A
+      <br />
+
+      <Link to={"/"}>Go to P</Link>
+
+    </div>
+
+  )
+}
+
